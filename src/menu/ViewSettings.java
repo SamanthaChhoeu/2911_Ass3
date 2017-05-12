63556ed329bbb6be524a875ca15f98222cbfed93
SharonYang0824 <406765182@qq.com>
2017-05-13 01:55:21 +1000

    Update ViewSettings.java

--- src/menu/ViewSettings.java
+++ src/menu/ViewSettings.java
@@ -5,12 +5,18 @@ import java.util.Observer;
 
 import javax.swing.JButton;
 import javax.swing.JFrame;
+import javax.swing.JLabel;
 
 public class ViewSettings extends JFrame implements Observer {
 
     private static final long serialVersionUID = 1L;
     private ModelInterface mi;
     private JButton backButton;
+    private JButton btnon;
+    private JButton btnoff;
+    private JButton btnMusic1;
+    private JButton btnMusic2;
+    private JLabel lb;
 
     public ViewSettings(ModelInterface mi) {
         
@@ -35,11 +41,47 @@ public class ViewSettings extends JFrame implements Observer {
         backButton.setBounds(1025, 600, 200, 50);
         this.add(backButton);
         
+        //create a label "music setting" 
+        lb = new JLabel("Music Setting",SwingConstants.CENTER);
+        this.add(lb);
+        lb.setBounds(98,100,100,100);
+        
+        //create below buttons to control and select music
+    	btnon = new JButton("On");
+    	btnoff = new JButton("Off");
+    	btnMusic1 = new JButton("Music1");
+    	btnMusic2 = new JButton("Music2");
+    	btnon.setBounds(100, 180, 100, 50);
+    	btnoff.setBounds(100, 280, 100, 50);
+    	btnMusic1.setBounds(100, 380, 100, 50);
+    	btnMusic2.setBounds(100, 480, 100, 50);
+    	this.add(btnon);
+    	this.add(btnoff);
+    	this.add(btnMusic1);
+    	this.add(btnMusic2);
+        
     }
 
     public JButton getBackButton() {
         return backButton;
     }
+    
+    public JButton getbtnon() {
+        return btnon;
+    }
+    
+    public JButton getbtnoff() {
+        return btnoff;
+    }
+    
+    public JButton getbtnMusic1() {
+        return btnMusic1;
+    }
+    
+    public JButton getbtnMusic2() {
+        return btnMusic2;
+    }
+
 
     @Override
     public void update(Observable o, Object arg) {
